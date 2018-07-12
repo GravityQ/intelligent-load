83491aeb8ec164dc9dda9b1041a1ce5dc9cc9623
junlin.qi <julin.qi@bitmain.com>
2018-07-12 13:58:42 +0800

    Initial commit

---
+++ src/main/java/com/usst/IntelligentLoadApplication.java
@@ -0,0 +1,12 @@
+package com.usst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentLoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentLoadApplication.class, args);
+	}
+}
